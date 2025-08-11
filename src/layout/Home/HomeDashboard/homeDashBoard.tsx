ae6f442f88e5ac1c46b6d00cbacc82bf15ffb1ac
vynguyenle <vylenguyen@tcom.vn>
2025-08-12 01:14:41 +0700

    feat: charts

--- src/layout/Home/HomeDashboard/homeDashBoard.tsx
+++ src/layout/Home/HomeDashboard/homeDashBoard.tsx
@@ -1,10 +1,47 @@
 import { Col, DatePicker, Form, Row } from "antd";
 import "./homeDashBoard.scss";
+import { ChartComponent } from "../../../component/Chart/chart";
 
 const { RangePicker } = DatePicker;
 
 export const HomeDashboard = () => {
   const [form] = Form.useForm();
+
+  const salesData = [
+    { x: "Tháng 1", y: 120 },
+    { x: "Tháng 2", y: 150 },
+    { x: "Tháng 3", y: 180 },
+    { x: "Tháng 4", y: 200 },
+    { x: "Tháng 5", y: 170 },
+    { x: "Tháng 6", y: 220 },
+  ];
+
+  const usersByRegionData = [
+    { x: "Hà Nội", y: 2500 },
+    { x: "TP.HCM", y: 3200 },
+    { x: "Đà Nẵng", y: 1800 },
+    { x: "Cần Thơ", y: 1200 },
+    { x: "Hải Phòng", y: 1600 },
+  ];
+
+  const scoreData = [
+    { x: "Toán", y: 8.5 },
+    { x: "Văn", y: 7.8 },
+    { x: "Anh", y: 9.2 },
+    { x: "Lý", y: 8.0 },
+    { x: "Hóa", y: 7.5 },
+    { x: "Sinh", y: 8.8 },
+  ];
+
+  const productSalesData = [
+    { x: "iPhone", y: 450 },
+    { x: "Samsung", y: 380 },
+    { x: "Xiaomi", y: 290 },
+    { x: "Oppo", y: 220 },
+    { x: "Vivo", y: 180 },
+    { x: "Realme", y: 150 },
+  ];
+
   return (
     <div className="dash-board">
       <Form form={form} className="dash-board__form">
@@ -40,8 +77,28 @@ export const HomeDashboard = () => {
         </Row>
 
         <Row gutter={[12, 12]} className="dash-board__section-3">
-          <Col className="dash-board__section-3-col" span={12}></Col>
-          <Col className="dash-board__section-3-col" span={12}></Col>
+          <Col className="dash-board__section-3-col" span={12}>
+            <ChartComponent
+              color="#f8f8f8"
+              orientation="horizontal"
+              data={salesData}
+            />
+          </Col>
+          <Col className="dash-board__section-3-col" span={12}>
+            <ChartComponent color="#f8f8f8" data={usersByRegionData} />
+          </Col>
+        </Row>
+        <Row gutter={[12, 12]} className="dash-board__section-4">
+          <Col className="dash-board__section-4-col" span={12}>
+            <ChartComponent
+              color="#f8f8f8"
+              orientation="horizontal"
+              data={scoreData}
+            />
+          </Col>
+          <Col className="dash-board__section-4-col" span={12}>
+            <ChartComponent color="#f8f8f8" data={productSalesData} />
+          </Col>
         </Row>
       </Form>
     </div>
